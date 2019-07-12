09557f4767ba4e4b4c67407d1dafd505e27178ca
Erik <erikborella@gmail.com>
2019-07-12 20:58:34 -0300

    MenuHelper create

--- menus/include/menuHelper.h
+++ menus/include/menuHelper.h
@@ -5,4 +5,8 @@
 #ifndef C_HANOITOWER_MENUHELPER_H
 #define C_HANOITOWER_MENUHELPER_H
 
+void clearScreen(void);
+bool verifyOption(int minOption, int maxOption, int option);
+int menu(char* message, char* prompt, char* error, int minOption, int maxOption);
+
 #endif //C_HANOITOWER_MENUHELPER_H
--- menus/src/menuHelper.c
+++ menus/src/menuHelper.c
@@ -2,4 +2,39 @@
 // Created by erikborella on 12/07/19.
 //
 
-#include "menuHelper.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "../include/menuHelper.h"
+
+void clearScreen(void) {
+    for (int i = 0; i < 100; i++) {
+        printf("\n");
+    }
+}
+
+bool verifyOption(int minOption, int maxOption, int option) {
+    if (option < minOption || option > maxOption) {
+        return false;
+    }
+    return true;
+}
+
+int menu(char* message, char* prompt, char* error, int minOption, int maxOption) {
+    int option = 0;
+    bool checker = false;
+
+    while (!checker) {
+        clearScreen();
+        printf("%s\n", message);
+        printf("%s", prompt);
+
+        scanf("%d", &option);
+        checker = verifyOption(minOption, maxOption, option);
+        if (!checker) {
+            printf("%s\n", error);
+        }
+    }
+
+    return option;
+}
