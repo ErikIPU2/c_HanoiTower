5afab6b0e6a62c5542b334316f3628c56f4781d4
Erik <erikborella@gmail.com>
2019-07-12 20:54:57 -0300

    create menuHelper

---
+++ menus/include/menuHelper.h
@@ -0,0 +1,8 @@
+//
+// Created by erikborella on 12/07/19.
+//
+
+#ifndef C_HANOITOWER_MENUHELPER_H
+#define C_HANOITOWER_MENUHELPER_H
+
+#endif //C_HANOITOWER_MENUHELPER_H
---
+++ menus/src/menuHelper.c
@@ -0,0 +1,5 @@
+//
+// Created by erikborella on 12/07/19.
+//
+
+#include "menuHelper.h"
