3487a1fc675583c79148b29692c02393857c520e
Erik <erikborella@gmail.com>
2019-07-13 15:49:33 -0300

    Create menu's files

---
+++ menus/include/menu.h
@@ -0,0 +1,8 @@
+//
+// Created by erikborella on 13/07/19.
+//
+
+#ifndef C_HANOITOWER_MENU_H
+#define C_HANOITOWER_MENU_H
+
+#endif //C_HANOITOWER_MENU_H
---
+++ menus/src/menu.c
@@ -0,0 +1,5 @@
+//
+// Created by erikborella on 13/07/19.
+//
+
+#include "menu.h"
